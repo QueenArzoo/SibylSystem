a2da050e9f3f18e46639824408e5c5af3a01ebc4
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-10-18 00:08:29 +0800

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -1,10 +1,10 @@
 from typing import Union, Optional
 
-API_ID: int = 12345
-API_HASH: str = "XYZ"
-STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Sibyl_logs: Union[str, int] = 123456789
-Sibyl_approved_logs: Union[str, int] = 123456789
+API_ID: int = 8196691
+API_HASH: str = "857d32fc610f269a65ca8541731ba4c4"
+STRING_SESSION: str = "1AZWarzwBu2YhIEe-Xv-I8lIAMYLEIimRi7p7Dd0oxc-v-VOgLMPYv71tO4AqgKRsL5LhANGTwhvMM6ucG6hQj1QkPRbvsBb23KLiT2JVJLQ-rjtxru_thEQTf4evyRU20dQuGSg3GgNt_xwNrssPQHjL8uqLcxpJiVdOFL4oYSp3Iz2E3NOgNZ-aqNfwncVtghiKcezzlSVdwbx902PoCeJjrORsJexQZjVRxhqp2h6VjUogZORaJt7ynroNB2xNdg933HCEoS_L4yBQGYg85EPrSLTSE0cLHNPbaVGcSWrUtff4Xy0ITWo85h63hYDTmdLanElshYsN2nMXoYAs-TggmmY-TgQ="
+Sibyl_logs: Union[str, int] = 785963102
+Sibyl_approved_logs: Union[str, int] = 632936601
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
 MONGO_DB_URL: str = ""
